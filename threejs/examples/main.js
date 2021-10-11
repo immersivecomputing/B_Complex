4a53ed138454f8756ddeb8d8891120b22e0d30d1
prat615 <martin.pratt@pnnl.gov>
2021-10-11 09:54:11 -0700

    update material and add light

--- threejs/examples/main.js
+++ threejs/examples/main.js
@@ -16,7 +16,12 @@ function init(){
 		10000
 	);
 
-	
+	var light = getDirectionalLight(1);
+	scene.add(spotLight);
+	gui.add(light, 'intensity', 0, 10);
+	gui.add(light.position, 'x', 0, 20);
+	gui.add(light.position, 'y', 0, 20);
+	gui.add(light.position, 'z', 0, 20);
 
 	var renderer = new THREE.WebGLRenderer();
 	renderer.setSize(window.innerWidth, window.innerHeight);
@@ -26,8 +31,8 @@ function init(){
 
 	update(renderer, scene, camera, controls);
 	
-	var material = new THREE.MeshBasicMaterial({
-		color: 0xffffff,
+	var material = new THREE.MeshPhongMaterial({
+		color: 'rgb(120,120,120)',
 		side: THREE.DoubleSide
 	});
 	const loader = new OBJLoader();
@@ -39,29 +44,29 @@ function init(){
 		object.traverse( function(child){
 			if (child.isMesh) {
 				child.material = material;
-
 			}
 		});
 
 		var middle = new THREE.Vector3();
 		var bbox = new THREE.Box3().setFromObject(object);
 
-		console.log(bbox);
-
 		middle.x = (bbox.max.x + bbox.min.x) / 2;
 		middle.y = (bbox.max.y + bbox.min.y) / 2;
 		middle.z = (bbox.max.z + bbox.min.z) / 2;
 
-
-		camera.position.set(bbox.min.x, bbox.min.y, bbox.min.z);
+		camera.position.set(bbox.min.x, bbox.max.y, bbox.min.z);
 		camera.lookAt(middle.x, middle.y, middle.z);
 		controls.target.set(middle.x, middle.y, middle.z);
-
 
 		scene.add(object);
 
 	} );
 	
+}
+
+function getDirectionalLight(intensity) {
+	var light = new THREE.DirectionalLight(0xffffff, intensity)
+	light.castShadow = true;
 }
 
 function update(renderer, scene, camera, controls) {
