b9158d259f364199fc39d347a34fb6d4bbc20282
prat615 <martin.pratt@pnnl.gov>
2021-10-14 08:48:25 -0700

    add stencil example for testing

--- threejs/examples/main.js
+++ threejs/examples/main.js
@@ -185,6 +185,45 @@ function init(){
 	console.log(scene);
 }
 
+function createPlaneStencilGroup(geometry, plane, renderOrder) {
+
+	const group = new THREE.Group();
+	const baseMat = new THREE.MeshBasicMaterial();
+	baseMat.depthWrite = false;
+	baseMat.depthTest = false;
+	baseMat.colorWrite = false;
+	baseMat.stencilWrite = true;
+	baseMat.stencilFunc = THREE.AlwaysStencilFunc;
+
+	// back faces
+	const mat0 = baseMat.clone();
+	mat0.side = THREE.BackSide;
+	mat0.clippingPlanes = [plane];
+	mat0.stencilFail = THREE.IncrementWrapStencilOp;
+	mat0.stencilZFail = THREE.IncrementWrapStencilOp;
+	mat0.stencilZPass = THREE.IncrementWrapStencilOp;
+
+	const mesh0 = new THREE.Mesh(geometry, mat0);
+	mesh0.renderOrder = renderOrder;
+	group.add(mesh0);
+
+	// front faces
+	const mat1 = baseMat.clone();
+	mat1.side = THREE.FrontSide;
+	mat1.clippingPlanes = [plane];
+	mat1.stencilFail = THREE.DecrementWrapStencilOp;
+	mat1.stencilZFail = THREE.DecrementWrapStencilOp;
+	mat1.stencilZPass = THREE.DecrementWrapStencilOp;
+
+	const mesh1 = new THREE.Mesh(geometry, mat1);
+	mesh1.renderOrder = renderOrder;
+
+	group.add(mesh1);
+
+	return group;
+
+}
+
 function loadCSV2D(fileName, rowMin, rowMax, threeGroup) {
 	const csvloader = new THREE.FileLoader();
 	csvloader.load(fileName,
@@ -260,6 +299,7 @@ function loadOBJ(fileName, renderOrder, objContainer) {
 
 		objContainer.add(object);
 		setCameraAndBBox(objContainer);
+
 	});
 }
 
