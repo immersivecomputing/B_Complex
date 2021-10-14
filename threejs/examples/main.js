f951cec04dc38dc0c1a678b2f0fa9de9a200f9be
prat615 <martin.pratt@pnnl.gov>
2021-10-14 14:08:24 -0700

    switch lookat positions

--- threejs/examples/main.js
+++ threejs/examples/main.js
@@ -520,8 +520,8 @@ function update(renderer, scene, camera, controls) {
 		po.position.set(planeHelper.position.x, planeHelper.position.y, planeHelper.position.z);
 
 		po.lookAt(
-			po.position.x + plane.normal.x,
-			po.position.y - plane.normal.y,
+			po.position.x - plane.normal.x,
+			po.position.y + plane.normal.y,
 			po.position.z - plane.normal.z,
 		);
 
